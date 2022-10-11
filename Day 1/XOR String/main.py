cc843caee0eb831ee7ab54747dad9d670cfb24c9
Nithik Narayan M <192012010.sse@saveetha.com>
2022-10-11 08:13:31 +0530

    removed Files

--- Day 1/XOR String/main.py
+++
@@ -1,4 +0,0 @@
-text = input("Enter a text: ")
-for i in text:
-    result = ord(i)^0
-    print(f"{result}", end="\n")
--- Day 4/SHA1/main.py
+++
@@ -1,7 +0,0 @@
-import hashlib
-
-plain = input("Plain Text: ")
-hash = hashlib.sha1(plain.encode())
-
-print("SHA1 Equivalent of String: {}".format(hash.digest()))
-print("Hexadecimal Equicalent of SHA1: {}".format(hash.hexdigest()))
